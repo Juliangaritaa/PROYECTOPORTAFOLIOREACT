782d64e390805ce00dafc227f42544ee9106b385
Julian Felipe Angarita Alarcon <julian.angarita11@soy.sena.edu.co>
2025-07-15 14:13:33 -0500

    m

--- PROYECTOPORTAFOLIOREACT/vite.config.ts
+++ PROYECTOPORTAFOLIOREACT/vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+'@vitejs/plugin-react-swc' // Importing the React plugin for Vite
+'vite-plugin-gh-|pages' // Importing the GitHub Pages plugin for Vite
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/PROYECTOPORTAFOLIOREACT/', // Base path for GitHub Pages
   plugins: [react()],
 })
